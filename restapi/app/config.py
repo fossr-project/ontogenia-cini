1a7ae29a0656f75e96da2a2ca3ef22d636fca5bf
Anna Sofia Lippolis <anna.sofial96@gmail.com>
2025-05-01 20:46:19 +0200

    Update config.py

--- restapi/app/config.py
+++ restapi/app/config.py
@@ -2,7 +2,7 @@ import os
 
 # Global configuration settings
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY", "insert_key")
-DEFAULT_DATASET = os.getenv("DEFAULT_DATASET", "benchmarkdataset.csv")
-EXTERNAL_CQ_GENERATION_URL = os.getenv("EXTERNAL_CQ_GENERATION_URL", "http://127.0.0.1:8001/newapi")
+DEFAULT_DATASET = os.path.join(os.path.dirname(__file__), "benchmarkdataset.csv")
+EXTERNAL_CQ_GENERATION_URL = os.getenv("EXTERNAL_CQ_GENERATION_URL", "http://127.0.0.1:8001/newapi") #e.g., your personal url
 HEATMAP_OUTPUT_FOLDER = os.getenv("HEATMAP_OUTPUT_FOLDER", "heatmaps")
 RESULTS_DIR = os.getenv("RESULTS_DIR", "results")
